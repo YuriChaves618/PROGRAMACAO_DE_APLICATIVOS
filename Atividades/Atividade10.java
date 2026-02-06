a55f5d87cb38166308e8727a9b5a9ba0ba678b3c
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-06 16:28:14 -0300

    Add Atividade10.java with basic structure

---
+++ Atividades/Atividade10.java
@@ -0,0 +1,13 @@
+package AtividadeNivel04;
+import java.util.Scanner;
+
+public class Atividade10 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner (System.in);
+
+        boolean isHomem = true;
+
+        System.out.println(isHomem);
+
+    }
+}

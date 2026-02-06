45d9c494e14b51d347646e64db4bd2ccfc130856
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-06 15:35:21 -0300

    Add TiposVariaveis class to demonstrate variable types

---
+++ Atividades/TiposDeVariaveis.java
@@ -0,0 +1,30 @@
+package AulasProgramacaoApps;
+import java.util.Scanner;
+
+public class TiposVariaveis {
+
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+
+        int idade = 14;
+
+        System.out.println("A idade é: "+idade);
+
+        double altura = 1.62;
+
+        System.out.println("\nA altura é: "+altura);
+
+        boolean isEstudante = true;
+        System.out.println("\nÉ estudante ? "+isEstudante);
+
+        char genero = 'M';
+
+        System.out.println("\nO Genêro é: "+genero);
+
+        String nome = "João";
+
+        System.out.println("\nO nome é: "+nome);
+
+
+    }
+}

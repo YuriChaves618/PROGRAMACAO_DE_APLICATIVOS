55ecea913f1b393fa1c3b17c3a2e65488af6e1a0
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-06 16:04:09 -0300

    Add Atividade05.java for user input demonstration

---
+++ Atividades/Atividade05.java
@@ -0,0 +1,18 @@
+package AtividadesNivel02;
+import java.util.Scanner;
+
+public class Atividade05 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        double n1 = 0;
+
+        System.out.println("Informe Um Valor");
+        n1 = scanner.nextDouble();
+
+        System.out.println("O valor informado foi: "+n1);
+
+
+
+    }
+}

74b201a23dd1f9fe5d74bfcc617c029700b23fe2
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-06 16:02:47 -0300

    Add Atividade04.java to calculate double of a number

---
+++ Atividades/Atividade04.java
@@ -0,0 +1,19 @@
+package AtividadesNivel01;
+import java.util.Scanner;
+
+public class Atividade04 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = 0;
+
+        System.out.println("Informe um número ");
+        n1 = scanner.nextInt();
+
+        n1*=2;
+
+        System.out.println("O Dobro Desse Número é: "+n1);
+
+
+    }
+}

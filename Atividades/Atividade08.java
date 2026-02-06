2b5cc2067abf3953926fbd5fefe82c890de43858
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-06 16:06:52 -0300

    Add Atividade08.java for user greeting

---
+++ Atividades/Atividade08.java
@@ -0,0 +1,16 @@
+package AtividadesNivel03;
+import java.util.Scanner;
+
+public class Atividade08 {
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nome = "";
+        System.out.println("Qual o seu nome ?");
+        nome = scanner.next();
+
+        System.out.println("\nBem Vindo "+nome+"!");
+
+    }
+}

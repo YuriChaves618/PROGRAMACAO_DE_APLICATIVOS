27135bb83e179083dfbb76e9dce83dc66d94d6b2
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-06 15:31:56 -0300

    Update Atividade01.java

--- Atividades/Atividade01.java
+++ Atividades/Atividade01.java
@@ -1,1 +1,13 @@
+package AtividadesNivel01;
 
+public class Atividade_1 {
+    //Declare uma variavel do tipo int, atribua um valor a ela e mostre o valor na tela.
+
+    public static void main(String[]args){
+
+        int Idade = 20;
+
+        System.out.println(Idade);
+
+    }
+}

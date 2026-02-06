b08e2ae9cf89db2c39980ad36dc562ba0a511292
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-06 16:28:52 -0300

    Add Atividade11.java for boolean input handling

---
+++ Atividades/Atividade11.java
@@ -0,0 +1,17 @@
+package AtividadeNivel04;
+import java.util.Scanner;
+
+public class Atividade11 {
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isValor;
+
+        System.out.println("Digite um valor Booleano");
+        isValor = scanner.nextBoolean();
+
+        System.out.println("\nValor Booleano Digitado: "+isValor);
+    }
+
+}

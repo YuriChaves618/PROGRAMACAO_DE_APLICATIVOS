32e02051d4e1dfa35a3b15d83ffecf087e4875d6
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-06 16:02:18 -0300

    Add Atividade03.java for age input functionality

---
+++ Atividades/Atividade03.java
@@ -0,0 +1,20 @@
+package AtividadesNivel01;
+import java.util.Scanner;
+
+public class Atividade03 {
+    public static void main(String []args){
+        Scanner scanner = new Scanner (System.in);
+
+        int idade = 0;
+
+        System.out.println("Informe sua idade: ");
+        idade = scanner.nextInt();
+
+        System.out.println("Sua idade é: "+idade);
+
+        System.out.println("\nIdade Recebida Com Sucesso!");
+
+
+
+    }
+}

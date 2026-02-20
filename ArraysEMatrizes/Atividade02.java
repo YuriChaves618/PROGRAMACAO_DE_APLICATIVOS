e1b77a3b79c718962776e57f1e3dc7be173bb39d
YuriChaves618 <yuridejesus0976@gmail.com>
2026-02-20 16:03:12 -0300

    Add Atividade02_02 to calculate sum of even numbers

---
+++ ArraysEMatrizes/Atividade02.java
@@ -0,0 +1,34 @@
+package Atividade02_Arrays;
+import java.util.Scanner;
+
+public class Atividade02_02 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int [] numeros = new int[4];
+        int soma = 0;
+
+        for(int i = 0; i < numeros.length; i++){
+            int valores = 0, divisao = 0;
+            System.out.println("Digite o "+(i+1)+" Número");
+            valores = scanner.nextInt();
+
+            try{
+                numeros[i] = valores;
+                divisao = valores % 2;
+
+                if( divisao == 0 ){
+                    soma += numeros[i];
+                }
+            }catch(Exception e){
+                System.out.println("Valor Inválido! Tente Novamente");
+                scanner.nextLine();
+                continue;
+            }
+
+        }
+
+            System.out.println("A soma dos números pares digitados é: "+soma);
+
+
+    }
+}

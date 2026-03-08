1e6c578c11d6d98cfab3a81cb60a5b0bf286abce
YuriChaves618 <yuridejesus0976@gmail.com>
2026-03-08 15:48:09 -0300

    Update ContaBancaria.java

--- Conta/ContaBancaria.java
+++ Conta/ContaBancaria.java
@@ -1,34 +1,32 @@
-package ContaBancaria;
-import java.util.Scanner;
+package Banco;
 
-public class Conta_Bancaria {
+import java.sql.SQLOutput;
 
-    Scanner scanner = new Scanner(System.in);
+public class ContaBancaria {
 
-    private String numero_conta = "";
+    private String numeroConta = "";
     private String titular = "";
     private double saldo = 0;
 
-    public Conta_Bancaria(){
+    public ContaBancaria(){
 
     }
 
-    public Conta_Bancaria(String numero_conta, String titular, double saldo){
-        this.numero_conta = numero_conta;
-        this.titular = titular;
+    public ContaBancaria(String numeroConta, String titular, double saldo){
         this.saldo = saldo;
-
+        this.titular = titular;
+        this.numeroConta = numeroConta;
     }
 
-    public String getNumero_Conta(){
-        return this.numero_conta;
+    public String getNumeroConta(){
+        return this.numeroConta;
     }
 
-    public void setNumero_Conta(String numero_conta){
-        this.numero_conta = numero_conta;
+    public void setNumeroConta(String numeroConta){
+        this.numeroConta = numeroConta;
     }
 
-    public String getTitulart(){
+    public String getTitular(){
         return this.titular;
     }
 
@@ -40,73 +38,97 @@ public class Conta_Bancaria {
         return this.saldo;
     }
 
-    public void setSaldo(double saldo){
+    public void setSaldo(){
         this.saldo = saldo;
     }
 
-    public void Deposito(){
+    public void Deposito(double valor_saldo){
 
-        int valorDeposito = 0;
-        System.out.println("Informe o valor que deseja depositar");
-        setSaldo(scanner.nextDouble());
-
+        if(valor_saldo < 0){
+            System.out.println("Impossível realizar um deposito negativo!");
+            return;
+        }else{
+            this.saldo = valor_saldo;
+            System.out.println("Depósito Realizado com Sucesso");
+        }
     }
 
-    public void Sacar(){
-        int divisao = 0, valorsaque = 0;
-        int nota100 = 0, nota50 = 0, nota20 = 0, nota10 = 0, nota5 = 0,nota2 = 0, nota1 = 0;
+    public void Sacar(double valor_saque){
 
-            System.out.println("Qual valor você deseja sacar ?");
-            valorsaque = scanner.nextInt();
+        int divisao = 0;
+        int notas100 = 0, notas50 = 0, notas20 = 0, notas10 = 0, notas5 = 0;
+        int notas2 = 0, notas1 = 0;
 
+        if( valor_saque > this.saldo){
+            System.out.println("Saldo insuficiente");
+            return;
+        }
 
-            if(valorsaque > getSaldo()){
-                System.out.println("Saldo Indisponível");
-            }
+        if(valor_saque >= 100){
+            divisao = (int)valor_saque / 100;
+            valor_saque = valor_saque - (divisao * 100);
+            notas100 = divisao;
+        }
 
-                if (valorsaque >= 100) {
-                    divisao = valorsaque / 100;
-                    nota100 = divisao;
-                    valorsaque = valorsaque - (divisao * 100);
-                }
+        if(valor_saque >= 50 && valor_saque < 100){
+            divisao = (int)valor_saque / 50;
+            valor_saque = valor_saque - (divisao * 50);
+            notas50 = divisao;
+        }
 
-                if(valorsaque >= 50 && valorsaque < 100){
-                    divisao = valorsaque / 50;
-                    nota50 = divisao;
-                    valorsaque = valorsaque - (divisao * 50);
-                }
+        if(valor_saque >= 20 && valor_saque < 50){
+            divisao = (int)valor_saque / 20;
+            valor_saque = valor_saque - (divisao * 20);
+            notas20 = divisao;
+        }
 
-                if(valorsaque >= 20 && valorsaque < 50){
-                    divisao = valorsaque / 20;
-                    nota20 = divisao;
-                    valorsaque = valorsaque - (divisao * 20);
-                }
+        if(valor_saque >= 10 && valor_saque < 20){
+            divisao = (int)valor_saque / 10;
+            valor_saque = valor_saque - (divisao * 10);
+            notas10 = divisao;
+        }
 
-                if(valorsaque >= 10 && valorsaque < 20){
-                    divisao = valorsaque / 10;
-                    nota10 = divisao;
-                    valorsaque = valorsaque - (divisao * 10);
+        if(valor_saque >= 5 && valor_saque < 10){
+            divisao = (int)valor_saque / 5;
+            valor_saque = valor_saque - (divisao * 5);
+            notas5 = divisao;
+        }
 
-                }
-                if(valorsaque >= 5 && valorsaque < 10){
-                    divisao = valorsaque/5;
-                    nota5 = divisao;
-                    valorsaque = valorsaque - (divisao * 5);
-                }
-                if(valorsaque >= 2 && valorsaque < 5){
-                    divisao = valorsaque/2;
-                    nota2 = divisao;
-                    valorsaque = valorsaque - (divisao * 2);
-                }
-                if(valorsaque >= 1 && valorsaque < 2){
-                    divisao = valorsaque/1;
-                    nota1 = divisao;
-                    valorsaque = valorsaque - (divisao * 1);
-                }
+        if(valor_saque >= 2 && valor_saque < 5){
+            divisao = (int)valor_saque / 2;
+            valor_saque = valor_saque - (divisao * 2);
+            notas2 = divisao;
+        }
 
-                System.out.println("Você vai receber "+nota100+" notas de 100| "+nota50+" notas de 50 | "+nota20+" notas de 20 | "+nota10+" notas de 10 | "+nota5+" notas de 5| "
-                +nota2+" notas de 2 e "+nota1+" notas de 1");
+        if(valor_saque >= 1 && valor_saque < 2){
+            divisao = (int)valor_saque / 1;
+            valor_saque = valor_saque - (divisao * 1);
+            notas1 = divisao;
+        }
 
-            }
+        System.out.println("Saque Realizado com Sucesso, Você receberá: ");
+        System.out.println(notas100+" Notas de 100");
+        System.out.println(notas50+ " Notas de 50");
+        System.out.println(notas20+ " Notas de 20");
+        System.out.println(notas10+ " Notas de 10");
+        System.out.println(notas5+  " Notas de 5");
+        System.out.println(notas2+  " Notas de 2");
+        System.out.println(notas1+  " Notas de 1");
+    }
 
-               }
+    public void ValidaNumeroConta(String Numero_Conta){
+        if(this.numeroConta == Numero_Conta){
+            System.out.println("Credencial Correta!");
+        }else{
+            System.out.println("Credencial Incorreta!");
+        }
+    }
+
+    public void ValidaNome(String Nome){
+        if(this.titular == Nome){
+            System.out.println("Credencial Correta!");
+        }else{
+            System.out.println("Credencial Incorreta!");
+        }
+    }
+}

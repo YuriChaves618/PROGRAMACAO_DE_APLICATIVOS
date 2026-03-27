08858cd14e916d7ed3f1290ba2290013822de00a
YuriChaves618 <yuridejesus0976@gmail.com>
2026-03-27 16:17:10 -0300

    Create Carro.java

---
+++ SistemaVeiculo/Carro.java
@@ -0,0 +1,32 @@
+package Encapsulamento.Sistema.Veiculos;
+
+public class Carro extends Veiculo{
+
+
+    public Carro(){
+
+    }
+
+    @Override
+    public void ligar(int op1){
+
+        Boolean chavePresencial = true;
+
+        if(op1 == 1){
+            chavePresencial = true;
+        }
+
+            if(op1 == 2){
+                chavePresencial = false;
+        }
+
+        if(chavePresencial == true){
+            System.out.println("Veículo ligando...");
+        }else{
+            System.out.println("Impossível ligar o veículo sem a chave");
+        }
+
+
+    }
+
+}

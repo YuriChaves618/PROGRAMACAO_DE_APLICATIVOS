c8b936f29344e2f1dd77dcf40baddc40e4aee77c
YuriChaves618 <yuridejesus0976@gmail.com>
2026-03-27 16:19:54 -0300

    Create Gato.java

---
+++ SistemaAnimais/Gato.java
@@ -0,0 +1,10 @@
+package Encapsulamento.Sistema.Animais;
+
+public class Gato extends Animal {
+
+    @Override
+    public void emitirSom(){
+        System.out.println("O gato faz meow");
+    }
+
+}

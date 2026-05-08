dae55e1f536ed2266b9c5f9b2b48610bf21233a9
YuriChaves618 <yuridejesus0976@gmail.com>
2026-05-08 16:08:31 -0300

    Create Pessoa.java

---
+++ Av1_Gestao_Academia/Pessoa.java
@@ -0,0 +1,32 @@
+public class Pessoa {
+    private String nome;
+    private String cpf;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome:" + this.nome + ", CPF: " + this.cpf);
+    }
+}
